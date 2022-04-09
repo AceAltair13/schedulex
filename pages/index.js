07b39aabaefa0fc46049fcae2801bd1ff144833b
Tirth <tirththoria13@hotmail.com>
2022-04-09 11:38:23 +0530

    Add homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+import { Container } from '@mui/material'
+import Hero from '../components/Home/Hero'
+import Navbar from '../components/Home/Navbar'
+
 export default function Home() {
-  return <>Home</>
+  return (
+    <Container
+      maxWidth="xl"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <Navbar />
+      <Hero />
+    </Container>
+  )
 }
