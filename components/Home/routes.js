4f327290e182d22d2a60d90cd78abdaed79f4a15
Tirth <tirththoria13@hotmail.com>
2022-04-09 10:45:02 +0530

    Add babela and routes.js

---
+++ components/Home/routes.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        path: "/",
+        label: "Home",
+    },
+    {
+        path: "/institute-details",
+        label: "Institute Details",
+    },
+    {
+        path: "/about",
+        label: "About",
+    },
+];
